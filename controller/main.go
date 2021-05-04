2927ca616595dd969c68a09b8782fff8d7ac6254
omid <om.mo1375@gmail.com>
2021-05-04 11:59:58 +0430

    test rpc connection

--- controller/main.go
+++ controller/main.go
@@ -1,7 +1,40 @@
 package controller
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	pb "github.com/javadh75/dyane/protoc"
+	"google.golang.org/grpc"
+)
+
+const (
+	address     = "localhost:5000"
+	defaultName = "world"
+)
 
 func Run() {
 	fmt.Println("Running Controller")
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	// Contact the server and print out its response.
+	name := defaultName
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetMessage())
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,7 @@ go 1.16
 require (
 	github.com/spf13/cobra v1.1.3 // indirect
 	github.com/spf13/viper v1.7.1 // indirect
+	google.golang.org/grpc v1.37.0 // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
 )
